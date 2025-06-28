dca85d0533fa3ff866c46b54f6ed1a74862e7cd9
eielelle <eielelle915@gmail.com>
2025-06-28 22:59:32 +0800

    updated text in drawer

--- resources/views/components/dashboard-drawer.blade.php
+++ resources/views/components/dashboard-drawer.blade.php
@@ -2,7 +2,9 @@
     <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
     <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
       <!-- Sidebar content here -->
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
+      <li><a>@svg('bi-grid-fill') Overview</a></li>
+      <li><a>@svg('bi-wallet-fill') Manage Wallets</a></li>
+      <li><a>@svg('bi-receipt') Transactions</a></li>
+      <li><a>@svg('bi-ui-checks-grid') Categories</a></li>
     </ul>
   </div>
